03cfd4655d8e557b31c2513129947904c25a605f
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-04-08 07:45:41 +0400

    main.tf updated

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "web_server_test"
+  }
+}
